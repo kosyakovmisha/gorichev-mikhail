7ec1e9211da82deeef7e66969cebe9f403fbf3bf
Mihail <kosyakovmisha@yandex.ru>
2019-06-20 15:06:55 +0300

    add getKmProperty method

--- course-project/src/com/epam/brest2019/courses/Main.java
+++ course-project/src/com/epam/brest2019/courses/Main.java
@@ -1,6 +1,9 @@
 package com.epam.brest2019.courses;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class Main {
@@ -9,17 +12,19 @@ public class Main {
         BigDecimal weight;
         BigDecimal distance;
         BigDecimal pricePerKg = new BigDecimal("30");
-        BigDecimal pricePerKm = new BigDecimal("50");
+        BigDecimal pricePerKm;
 
         System.out.println("Enter the weight in kilograms or 'q' for quit:");
         weight = getValue();
         System.out.println("Enter the distance in kilometers or 'q' for quit:");
         distance = getValue();
 
+        pricePerKm = getKmProperty(distance);
+
         System.out.println("weight = " + weight);
         System.out.println("distance = " + distance);
         System.out.println("pricePerKg = " + pricePerKg);
-        System.out.println("pricePerKm =" + pricePerKm);
+        System.out.println("pricePerKm = " + pricePerKm);
         BigDecimal price = weight.multiply(pricePerKg).add(distance.multiply(pricePerKm));
         System.out.println("Price = " + price);
     }
@@ -54,5 +59,39 @@ public class Main {
             tempString = scanner.nextLine();
         }
         return new BigDecimal(tempString);
+    }
+
+    static BigDecimal getKmProperty(BigDecimal distance) {
+        BigDecimal less10 = new BigDecimal(100);
+        BigDecimal less100 = new BigDecimal(100);
+        BigDecimal less500 = new BigDecimal(100);
+        BigDecimal more500 = new BigDecimal(100);
+        try {
+            FileInputStream fileInputStream = new FileInputStream("./resources/price_per_km.properties");
+            Properties props = new Properties();
+            props.load(fileInputStream);
+            less10 = new BigDecimal(props.getProperty("less10"));
+            less100 = new BigDecimal(props.getProperty("less100"));
+            less500 = new BigDecimal(props.getProperty("less500"));
+            more500 = new BigDecimal(props.getProperty("more500"));
+
+        } catch (IOException ioe) {
+            System.out.println("Exception: " + ioe.getMessage());
+        }
+        System.out.println("Price up to 10 kilometers: " + less10);
+        System.out.println("Price from 10 to 100 kilometers: " + less100);
+        System.out.println("Price from 100 to 500 kilometers: " + less500);
+        System.out.println("Price over 500 kilometers: " + more500);
+
+        if(distance.compareTo(BigDecimal.valueOf(10)) <= 0) {
+            return less10;
+        } else if (distance.compareTo(BigDecimal.valueOf(100)) <= 0) {
+            return less100;
+        } else if (distance.compareTo(BigDecimal.valueOf(500)) <= 0){
+            return less500;
+        } else {
+            return more500;
+        }
+
     }
 }
