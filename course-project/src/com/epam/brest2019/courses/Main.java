34a39c40af999d7db5039b8c1a11f825bfad402e
Mihail <kosyakovmisha@yandex.ru>
2019-06-20 16:58:24 +0300

    Few changes in ui and adding constants

--- course-project/src/com/epam/brest2019/courses/Main.java
+++ course-project/src/com/epam/brest2019/courses/Main.java
@@ -7,11 +7,17 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class Main {
+
+    private static final String KG_PROPERTIES_FILE_PATH = "./resources/price_per_km.properties";
+    private static final BigDecimal DEFAULT_PRICE_PER_KM = new BigDecimal("100");
+    private static final BigDecimal DEFAULT_PRICE_PER_KG = new BigDecimal("30");
+
     public static void main(String[] args) {
+
 
         BigDecimal weight;
         BigDecimal distance;
-        BigDecimal pricePerKg = new BigDecimal("30");
+        BigDecimal pricePerKg = DEFAULT_PRICE_PER_KG;
         BigDecimal pricePerKm;
 
         System.out.println("Enter the weight in kilograms or 'q' for quit:");
@@ -19,17 +25,19 @@ public class Main {
         System.out.println("Enter the distance in kilometers or 'q' for quit:");
         distance = getValue();
 
-        pricePerKm = getKmProperty(distance);
+        pricePerKm = getKmProperty(distance, KG_PROPERTIES_FILE_PATH);
 
+        System.out.println("====================Values=====================");
         System.out.println("weight = " + weight);
         System.out.println("distance = " + distance);
         System.out.println("pricePerKg = " + pricePerKg);
         System.out.println("pricePerKm = " + pricePerKm);
+        System.out.println("===============================================");
         BigDecimal price = weight.multiply(pricePerKg).add(distance.multiply(pricePerKm));
         System.out.println("Price = " + price);
     }
 
-    static boolean isNumber(String value) {
+    private static boolean isNumber(String value) {
         if (value == null || value.isEmpty()) {
             return false;
         }
@@ -47,27 +55,31 @@ public class Main {
         return true;
     }
 
-    static BigDecimal getValue() {
+    private static BigDecimal getValue() {
         Scanner scanner = new Scanner(System.in);
         String tempString = scanner.nextLine();
         while (!isNumber(tempString) || tempString.equals("0")) {
             if (tempString.toLowerCase().equals("q")) {
                 System.out.println("Exit.");
                 System.exit(0);
+            } else if (tempString.equals("0")) {
+                System.out.println("The value must be greater than null. Type normal value:");
+            } else {
+                System.out.println(tempString + " - incorrect number format. Type your value:");
             }
-            System.out.println(tempString + " is wrong number format. Type your value:");
+
             tempString = scanner.nextLine();
         }
         return new BigDecimal(tempString);
     }
 
-    static BigDecimal getKmProperty(BigDecimal distance) {
-        BigDecimal less10 = new BigDecimal(100);
-        BigDecimal less100 = new BigDecimal(100);
-        BigDecimal less500 = new BigDecimal(100);
-        BigDecimal more500 = new BigDecimal(100);
+    private static BigDecimal getKmProperty(BigDecimal distance, String path) {
+        BigDecimal less10 = DEFAULT_PRICE_PER_KM;
+        BigDecimal less100 = DEFAULT_PRICE_PER_KM;
+        BigDecimal less500 = DEFAULT_PRICE_PER_KM;
+        BigDecimal more500 = DEFAULT_PRICE_PER_KM;
         try {
-            FileInputStream fileInputStream = new FileInputStream("./resources/price_per_km.properties");
+            FileInputStream fileInputStream = new FileInputStream(path);
             Properties props = new Properties();
             props.load(fileInputStream);
             less10 = new BigDecimal(props.getProperty("less10"));
@@ -77,17 +89,21 @@ public class Main {
 
         } catch (IOException ioe) {
             System.out.println("Exception: " + ioe.getMessage());
+            System.out.println("Exit.");
+            System.exit(0);
         }
+        System.out.println("===================Properties==================");
         System.out.println("Price up to 10 kilometers: " + less10);
         System.out.println("Price from 10 to 100 kilometers: " + less100);
         System.out.println("Price from 100 to 500 kilometers: " + less500);
         System.out.println("Price over 500 kilometers: " + more500);
+        System.out.println("===============================================");
 
         if(distance.compareTo(BigDecimal.valueOf(10)) <= 0) {
             return less10;
         } else if (distance.compareTo(BigDecimal.valueOf(100)) <= 0) {
             return less100;
-        } else if (distance.compareTo(BigDecimal.valueOf(500)) <= 0){
+        } else if (distance.compareTo(BigDecimal.valueOf(500)) <= 0) {
             return less500;
         } else {
             return more500;
